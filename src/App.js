44657bce72d203f4e3d82100c653158d034e1adb
jheyson <jheyson8529012@gmail.com>
2021-07-27 17:28:23 -0400

    segunda version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <body>
-
-      </body>
-    </div>
-  );
-}
-
+import React, { Component} from 'react';
+import logo from './img/background.png'
+import './App.css'; 
+import  Navigation from './components/Navigation';
+  class App extends Component{
+    render() {
+      return( 
+        <div className = "App"> 
+          <header className = "App-header">  
+           <h1 className = "App-title"> Welcome to my website</h1>
+            <img src={logo} className= "App-logo" alt= "logo" />
+          </header>
+          <Navigation  />
+        </div>
+      );
+    }
+  }
+  
 export default App;
+
