0c97005e0723d46cfc036f361a3939912fbadd53
jheyson <jheyson8529012@gmail.com>
2021-07-30 14:33:14 -0400

    programa final

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './App.css'
+import './assets/css/index.css';
+import './assets/css/App.css';
+import './assets/css/Baner.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
-  
+    
     <App />, document.getElementById('root')
+    
 );
+
 
 reportWebVitals();
